2647743f1f125b0519df4df1cfe68b270c12cc0e
latuanvinh1998 <latuanvinh1998@gmail.com>
2021-05-26 08:11:54 +0700

    Nothing Much

--- simple_tools.py
+++ simple_tools.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import torch
 
 def resize_bb(arr, width, height):
 
@@ -26,3 +27,14 @@ def E1(TP, TN, FP, FN):
 	mcc = (TP * TN - FP * FN)/math.sqrt ((TP + FP)*(TP + FN)*(TN + FP)*(TN + FN))
 
 	return accuracy, f1_score, mcc
+
+def select_triplets(anchor, positive, negative):
+	triplet = []
+	dist = np.copy(anchor.detach().cpu().numpy())
+
+	for i in range(len(anchor)):
+		dist_p = torch.dist(anchor[i], positive, 2).detach().cpu().numpy()
+		dist_n = torch.dist(anchor[i], negative, 2).detach().cpu().numpy()
+		dist[i] = dist_n + dist_p
+	print(dist)
+
--- train.py
+++ train.py
@@ -13,18 +13,19 @@ from neurnet import *
 
 import numpy as np
 import torch
+import json
 
-config_file = 'Detection/configs/swin/mask_rcnn_swin_tiny_patch4_window7_mstrain_480-800_adamw_3x_coco.py'
-checkpoint_file = '../Data/mask_rcnn_swin_tiny_patch4_window7.pth'
+# config_file = 'Detection/configs/swin/mask_rcnn_swin_tiny_patch4_window7_mstrain_480-800_adamw_3x_coco.py'
+# checkpoint_file = '../Data/mask_rcnn_swin_tiny_patch4_window7.pth'
 
-transform = transforms.Compose([transforms.Resize((300,300)), transforms.ToTensor(), transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),])
+# transform = transforms.Compose([transforms.Resize((300,300)), transforms.ToTensor(), transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),])
 
-model_det = init_detector(config_file, checkpoint_file, device='cuda:0')
-model_ef = EfficientNet.from_pretrained('efficientnet-b3').to(torch.device("cuda:0"))
-model_nlp = SentenceTransformer('stsb-mpnet-base-v2')
+# model_det = init_detector(config_file, checkpoint_file, device='cuda:0')
+# model_ef = EfficientNet.from_pretrained('efficientnet-b3').to(torch.device("cuda:0"))
+# model_nlp = SentenceTransformer('stsb-mpnet-base-v2')
 
-nn_image = Neural_Net(1536, 256)
-nn_sentence = Neural_Net(768, 256)
+# nn_image = Neural_Net(1536, 256)
+# nn_sentence = Neural_Net(768, 256)
 
 f = open('../Data/mmsys_anns/public_test_mmsys_final.json')
 labels = []
@@ -32,5 +33,8 @@ labels = []
 for line in f:
 	labels.append(json.loads(line))
 
+path = '../Data/' +  labels[0]['img_local_path']
 
+cap_1 = labels[0]['caption1_modified']
+cap_2 = labels[0]['caption2_modified']
 
