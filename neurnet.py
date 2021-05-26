0b00735c1a2b628e4c396495a5b07345f137fe66
latuanvinh1998 <latuanvinh1998@gmail.com>
2021-05-26 07:34:06 +0700

    Proposal

--- neurnet.py
+++ neurnet.py
@@ -3,9 +3,8 @@ from torch.nn import Dropout2d, Dropout, AvgPool2d, MaxPool2d, AdaptiveAvgPool2d
 import torch.nn.functional as F
 import torch
 import math
-import pdb
 
-class Img_Emb(Module):
+class Neural_Net(Module):
 	def __init__(self, in_features, embedding_size):
 		super(Img_Emb, self).__init__()
 		self.nn = Linear(in_features, embedding_size)
@@ -14,10 +13,10 @@ class Img_Emb(Module):
 		return out
 
 
-class Caption_Emb(Module):
-	def __init__(self, in_features, embedding_size):
-		super(Caption_Emb, self).__init__()
-		self.nn = Linear(in_features, embedding_size)
-	def forward(self, caption):
-		out = self.nn(caption)
-		return out
+# class Caption_Emb(Module):
+# 	def __init__(self, in_features, embedding_size):
+# 		super(Caption_Emb, self).__init__()
+# 		self.nn = Linear(in_features, embedding_size)
+# 	def forward(self, caption):
+# 		out = self.nn(caption)
+# 		return out
---
+++ train.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('Detection')
+
+from mmdet.apis import init_detector, inference_detector, show_result_pyplot
+from efficientnet_pytorch import EfficientNet
+from sentence_transformers import SentenceTransformer, util
+
+from torchvision import transforms
+from torch import nn
+from PIL import Image
+from simple_tools import *
+from neurnet import *
+
+import numpy as np
+import torch
+
+config_file = 'Detection/configs/swin/mask_rcnn_swin_tiny_patch4_window7_mstrain_480-800_adamw_3x_coco.py'
+checkpoint_file = '../Data/mask_rcnn_swin_tiny_patch4_window7.pth'
+
+transform = transforms.Compose([transforms.Resize((300,300)), transforms.ToTensor(), transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),])
+
+model_det = init_detector(config_file, checkpoint_file, device='cuda:0')
+model_ef = EfficientNet.from_pretrained('efficientnet-b3').to(torch.device("cuda:0"))
+model_nlp = SentenceTransformer('stsb-mpnet-base-v2')
+
+nn_image = Neural_Net(1536, 256)
+nn_sentence = Neural_Net(768, 256)
+
+f = open('../Data/mmsys_anns/public_test_mmsys_final.json')
+labels = []
+
+for line in f:
+	labels.append(json.loads(line))
+
+
+
